515989431052716a30d6f1186ef02b90f29f41d8
MacksonWelton <mackson.welton@gmail.com>
2020-02-06 21:17:47 -0300

    Correções e conclusão

--- semana4/funcoes-de-array/controlador-de-despesas/script.js
+++ semana4/funcoes-de-array/controlador-de-despesas/script.js
@@ -2,21 +2,21 @@ const todasAsDespesas = []
 
 class Despesas {
     constructor(valor, tipo, descricao) {
-    this.valor = valor;
-    this.tipo = tipo;
-    this.descricao = descricao;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.descricao = descricao;
     }
 }
 
 
 function novaDespesa() {
-    const valor = Number(document.querySelector("#valor").value);
+    const valor = parseInt(document.querySelector("#valor").value);
     const tipo = document.querySelector("#tipo-despesa").value;
     const descricao = document.querySelector("#descricao").value;
 
     if (valor === "" || tipo === "" || descricao === "") {
         alert("Campo não pode ficar em branco")
-    } {
+    } else {
         const despesa = new Despesas(valor, tipo, descricao);
         todasAsDespesas.push(despesa);
         listaTodasAsDespesas(todasAsDespesas);
@@ -24,46 +24,47 @@ function novaDespesa() {
 }
 
 function listaTodasAsDespesas(todasAsDespesas) {
-    const tabela = document.querySelector(".tabela-de-despesas > tbody");
 
-    for(let x = 0; x < todasAsDespesas.length; x++) {
+    const tabela = document.querySelector(".tabela-de-despesas > tbody");
+    tabela.innerHTML = ""
+
+    todasAsDespesas.forEach((item, index, array) => {
         tabela.innerHTML += `
         <tr>
-        <td>${todasAsDespesas[x].tipo}<td>
-        <td>${todasAsDespesas[x].valor}<td>
-        <td>${todasAsDespesas[x].descricao}<td>
+        <td>${todasAsDespesas[index].tipo}</td>
+        <td>${todasAsDespesas[index].valor}</td>
+        <td>${todasAsDespesas[index].descricao}</td>
         </tr>`
-    }
+    });
 }
 
 function filtrar() {
 
     const tipo = document.querySelector("#tipo-despesa-filtro").value;
-    const valorMin = Number(document.querySelector("#valorMinimo").value);
-    const valorMax = Number(document.querySelector("#valorMaximo").value);
+    const valorMin = parseInt(document.querySelector("#valorMinimo").value);
+    const valorMax = parseInt(document.querySelector("#valorMaximo").value);
 
     const resultadoDoFiltro = todasAsDespesas.filter((despesa) => {
         return despesa.tipo === tipo && despesa.valor >= valorMin && despesa.valor <= valorMax;
     })
 
-    listaExtrato(resultadoDoFiltro)
+        listaTodasAsDespesas(resultadoDoFiltro)
+        listaExtrato(resultadoDoFiltro)
 }
 
 function listaExtrato(resultadoDoFiltro) {
 
     let extrato = document.querySelector(".extrato");
 
-    let soma = 0;
 
-    for (let i = 0; i < resultadoDoFiltro.length; i++) {
-        soma += resultadoDoFiltro[i].valor;
-    }
-
+    const total = resultadoDoFiltro.reduce((soma, despesa) => soma += despesa.valor, 0);
 
     extrato.innerHTML += `<h2>Extrato</h2>`;
-    extrato.innerHTML += `<h3>Valor Total: ${soma}</h3>`;
+    extrato.innerHTML += `<h3>Valor Total: ${total}</h3>`;
+
 
 }
+
 
 function limparFiltros() {
     document.querySelector("#valorMinimo").value = "";
