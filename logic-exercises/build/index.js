132f0cdbbcc40b2b16fc736c42e10c748970ce63
MacksonWelton <mackson.welton@gmail.com>
2020-06-11 13:08:50 -0300

    Exercício de lógica

--- logic-exercises/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const exercise1_1 = require("./exercise1");
-const exercise3_1 = require("./exercise3");
-console.log(exercise1_1.firstFunc([10, 5, 6, 8, 10]));
-console.log(exercise1_1.biggerAndSmaller([2, 5, 6, 88, 10]));
-console.log(exercise3_1.equalTexts("Escola", "EsCola"));
-console.log(exercise3_1.similarTexts("Escola", "escola"));
